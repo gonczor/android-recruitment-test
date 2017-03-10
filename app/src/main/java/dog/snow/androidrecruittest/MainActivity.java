0425505a42af023f8ce690e4919a75cdb153a38b
gonczor <wiktor.gonczaronek@gmail.com>
2017-03-10 11:52:59 +0100

    setup implemented + refactoring

--- app/src/main/java/dog/snow/androidrecruittest/MainActivity.java
+++ app/src/main/java/dog/snow/androidrecruittest/MainActivity.java
@@ -1,5 +1,8 @@
 package dog.snow.androidrecruittest;
 
+import java.io.IOException;
+import java.util.List;
+
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,11 +10,15 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
-
-import java.io.IOException;
-import java.util.List;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 import dog.snow.androidrecruittest.database.ItemDatabaseHelper;
 import dog.snow.androidrecruittest.model.Item;
@@ -20,9 +27,7 @@ import dog.snow.androidrecruittest.rest.ApiClient;
 import dog.snow.androidrecruittest.rest.ApiInterface;
 import dog.snow.androidrecruittest.views.ErrorAdapter;
 import dog.snow.androidrecruittest.views.ItemAdapter;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+
 
 public class MainActivity extends AppCompatActivity {
     private ItemDatabaseHelper helper;
@@ -33,11 +38,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // helper MUST be created at the beginning since both downloadItems() and
+        // setupEditorAction() use database
         helper = new ItemDatabaseHelper.Builder()
                 .setContext(this)
                 .build();
         downloadItems();
-        showItemsFromDatabase();
+        showAllItems();
         setupSwipeContainer();
         setupEditorAction();
     }
@@ -54,7 +61,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setupEditorAction(){
+        final EditText search = (EditText) findViewById(R.id.search_et);
+        search.setOnEditorActionListener(new EditText.OnEditorActionListener() {
 
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_DONE){
+                    String searched = search.getText().toString();
+                    List<Item> foundItems = helper.selectMatchingItems(searched);
+                    showItems(foundItems);
+                }
+                return false;
+            }
+        });
     }
 
     private void downloadItems(){
@@ -99,8 +118,12 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void showItemsFromDatabase(){
+    private void showAllItems(){
         List<Item> items = helper.selectAllItems();
+        showItems(items);
+    }
+
+    private void showItems(List<Item> items){
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.response_rv);
         if(itemAdapter == null)
             itemAdapter = new ItemAdapter();
